8a8aa656c47356a90761ad5484714b4c4fc494f7
Jose Estrada <josecoreas@wireless-10-105-63-31.umd.edu>
2018-05-07 09:19:13 -0400

    Form
    Contact form udpate

--- contactform.php
+++ contactform.php
@@ -1,36 +1,15 @@
 <?php
-// Only process the form if $_POST isn't empty 
-if ( ! empty ($_POST)) )  {
-	
+$conn = new mysqli('localhost', 'root', 'root', 'on_campus');
+	if ($conn->connect_error) die($conn->connect_error);
 
-	// Connects to MYSQL
-	$mysqli = new mysqli( 'localhost', 'root', 'root', 'on_campus');
+	//store post data to array
+	$firstname = $_POST['first_name'];
+	$lastname = $_POST['last_name'];
+	$email = $_POST['email'];
+	$subject = $_POST['subject'];
+	$message = $_POST['message'];
 
-	// Checks the connection
-	if ($mysqli->connect_error ) {
-		die ('Connect Error: ' . $mysqli-> connect_errno . ':' . mysqli -> connect_error);
-
-	}
-
-	// Insert the data
-	$sql = "INSERT INTO form (first_name, last_name, email, subject, message) VALUES ( '{$mysqli->real_escape_string($_POST['first_name'])}',
-	'{$mysqli->real_escape_string($_POST['last_name'])}', '{$mysqli->real_escape_string($_POST['email'])}', 
-	'{$mysqli->real_escape_string($_POST['subject'])}', '{$mysqli->real_escape_string($_POST['message'])}' )";
-	$insert = $mysqli->query($sql);
-	 
-
-	// Print Response from MYSQL
-	if ($insert ) {
-		echo "Success! Row ID: {$mysqli->insert_id}";
-	} else {
-		die("Error: {$mysqli -> errno) : {$mysqli->error}");
-
-	}
-
-	// Closes the connection 
-	$mysqli-> close();
-}
-
-
+	$sql = "INSERT INTO form (first_name, last_name, email, subject, message) VALUES ('$firstname', '$lastname', '$email', '$subject', '$message');";
+	$result = $conn->query($sql);
 
 ?>
